4d664a3c27eb10e3058f4500c438455962dac060
Mikicaa97 <60716689+Mikicaa97@users.noreply.github.com>
2022-12-05 02:15:21 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,13 @@ var navSlide = () => {
 navSlide();
 
 
+$(document).ready(function(){
+    $('#fader').hover(
+        function(){
+            $(this).find('img:eq(1)').stop(true,true).fadeIn();
+        },
+        function(){
+            $(this).find('img:eq(1)').stop(true,true).fadeOut();
+        }
+    )
+});
